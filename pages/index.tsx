975f7a4b09bbcfda23ec1f475d9f7355b7744851
Shoota Kumano <shoota.dev@gmail.com>
2020-09-14 22:32:16 +0900

    construct markdown blog structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,61 @@
 import React from 'react'
-import styled from 'styled-components'
+import Head from 'next/head'
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import { CMS_NAME } from '../lib/constants'
+import Post from '../types/post'
 
-export default function Home() {
-  return <Title>My page</Title>
+type Props = {
+  allPosts: Post[]
+}
+
+const Index = ({ allPosts }: Props) => {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export default Index
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
 }
