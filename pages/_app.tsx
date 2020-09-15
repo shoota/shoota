4eaef21443886e38f7f72f3f2a69723edd191d5d
Shoota Kumano <shoota.dev@gmail.com>
2020-09-16 00:32:54 +0900

    add theme by copied rebass/preset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { AppProps } from 'next/app'
+import { ThemeProvider } from 'emotion-theming'
+
+import { theme } from './theme'
 // import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
