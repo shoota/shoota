07fa5f39a0aecfefb2b566d3e156fd755d709ba1
Shoota Kumano <shoota.dev@gmail.com>
2020-09-18 15:09:51 +0900

    refne skills component

--- components/atoms/FlexIconItem.tsx
+++ components/atoms/FlexIconItem.tsx
@@ -4,19 +4,16 @@ import { Text, Box } from 'rebass'
 type IconBoxProps = {
   name: string
   backgroundColor: string
+  color?: string
 }
 export const FlexIconBox: React.FC<IconBoxProps> = ({
   name,
   backgroundColor,
+  color,
   children,
 }) => (
-  <Box
-    p={1}
-    sx={{ textAlign: 'center', alignItems: 'center' }}
-    minWidth="88px"
-    width="20%"
-  >
-    <Box py={2} backgroundColor={backgroundColor}>
+  <Box p={1} sx={{ textAlign: 'center', alignItems: 'center' }} width="150px">
+    <Box py={2} color={color} backgroundColor={backgroundColor}>
       {children}
       <Text fontSize={[0, 0, 1]}>{name}</Text>
     </Box>
--- components/organisms/Skills.tsx
+++ components/organisms/Skills.tsx
@@ -10,6 +10,9 @@ import {
   Eslint,
   NodeDotJs,
   Webpack,
+  Babel,
+  Yarn,
+  StyledComponents,
 } from '@icons-pack/react-simple-icons'
 import { Box } from 'rebass'
 // import { Box as FlexBox } from 'reflexbox'
@@ -21,20 +24,23 @@ const size = 24
 
 export const Skills: React.FC = () => {
   return (
-    <Box mx="10%" sx={{ backgroundColor: 'background', width: '80%' }}>
+    <Box pl="8%" sx={{ backgroundColor: 'background', textAlign: 'center' }}>
       {/* <Text fontSize={[2, 2, 4]}>Skill set</Text> */}
       <FlexIconContainer>
         <FlexIconBox backgroundColor="#339933" name="Node.js">
           <NodeDotJs size={size} />
         </FlexIconBox>
-        <FlexIconBox backgroundColor="#000000" name="Next.js">
-          <NextDotJs size={size} />
+        <FlexIconBox backgroundColor="#2C8EBB" name="Yarn">
+          <Yarn size={size} />
         </FlexIconBox>
         <FlexIconBox backgroundColor="#007ACC" name="TypeScript">
           <Typescript size={size} />
         </FlexIconBox>
-        <FlexIconBox backgroundColor="#F7DF1E" name="JavaScript">
+        <FlexIconBox backgroundColor="#F7DF1E" name="JavaScript" color="text">
           <Javascript size={size} />
+        </FlexIconBox>
+        <FlexIconBox backgroundColor="#000000" name="Next.js">
+          <NextDotJs size={size} />
         </FlexIconBox>
         <FlexIconBox backgroundColor="#61DAFB" name="React">
           <ReactJs size={size} />
@@ -48,11 +54,17 @@ export const Skills: React.FC = () => {
         <FlexIconBox backgroundColor="#E10098" name="GraphQL">
           <Graphql size={size} />
         </FlexIconBox>
+        <FlexIconBox backgroundColor="#F9DC3E" name="Babel" color="text">
+          <Babel size={size} />
+        </FlexIconBox>
         <FlexIconBox backgroundColor="#4B32C3" name="ESLint">
           <Eslint size={size} />
         </FlexIconBox>
-        <FlexIconBox backgroundColor="#8DD6F9" name="Webpack">
+        <FlexIconBox backgroundColor="#8DD6F9" name="Webpack" color="text">
           <Webpack size={size} />
+        </FlexIconBox>
+        <FlexIconBox backgroundColor="#DB7093" name="styled-components">
+          <StyledComponents size={size} />
         </FlexIconBox>
       </FlexIconContainer>
     </Box>
