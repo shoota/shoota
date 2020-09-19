3b1c90c45c284350dd73b3c474c6b20193809e4b
Shoota Kumano <shoota.dev@gmail.com>
2020-09-20 01:11:08 +0900

    feat: improve nav bar

--- components/molecules/Navbar.tsx
+++ components/molecules/Navbar.tsx
@@ -2,22 +2,23 @@ import React from 'react'
 import { Flex, Text, Box, Link } from 'rebass'
 
 export const NavBar = () => (
-  <Flex pl={2} pr={6} alignItems="baseline" backgroundColor="background">
+  <Flex pl={2} pr={2} alignItems="baseline">
     <Text fontSize={[2, 4, 6]} p={2} fontWeight="bold">
       shoota.work
     </Text>
-    <Box mx="auto" />
-    <Link mx={1} fontSize={[0, 1, 2]} variant="nav" href="/">
-      Home
-    </Link>
-    <Link px={3} fontSize={[0, 1, 2]} variant="nav" href="/blog">
-      Blog
-    </Link>
-    <Link px={3} fontSize={[0, 1, 2]} variant="nav" href="#!">
-      WORKS
-    </Link>
-    <Link px={3} fontSize={[0, 1, 2]} variant="nav" href="/profile">
-      Profile
-    </Link>
+    <Box ml="auto">
+      <Link mx={1} fontSize={[0, 1, 2]} variant="nav" href="/">
+        Home
+      </Link>
+      <Link mx={1} fontSize={[0, 1, 2]} variant="nav" href="/blog">
+        Blog
+      </Link>
+      {/* <Link mx={1} fontSize={[0, 1, 2]} variant="nav" href="/works">
+        WORKS
+      </Link> */}
+      <Link mx={1} fontSize={[0, 1, 2]} variant="nav" href="/profile">
+        Profile
+      </Link>
+    </Box>
   </Flex>
 )
