5c297942b84b62b5865ad96bb64b335e868f9d2f
Shuta Kumano <shoota.dev@gmail.com>
2021-02-08 00:51:44 +0900

    revert emotion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppProps } from 'next/app'
-import { ThemeProvider } from '@emotion/react'
+import { ThemeProvider } from 'emotion-theming'
 
 import { theme } from '../theme'
 // import '../styles/index.css'
