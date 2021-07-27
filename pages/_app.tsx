117ca176dc2a1e8983bb8a2418ccecaeab4060b3
Shoota Kumano <shoota.dev@gmail.com>
2021-07-27 23:39:54 +0900

    apply arwes theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'emotion-theming'
 
+import { ArwesThemeProvider, StylesBaseline } from '@arwes/core'
+
 import { theme } from '../theme'
 // import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ArwesThemeProvider>
+      <StylesBaseline />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ArwesThemeProvider>
   )
 }
