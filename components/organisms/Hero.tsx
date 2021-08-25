ed5d82dd13cad2e31e60694a2142dff24dab8f7e
Shoota Kumano <shoota.dev@gmail.com>
2021-08-25 09:18:02 +0900

    colorize fiugre by background and opacity (#86)

--- components/organisms/Hero.tsx
+++ components/organisms/Hero.tsx
@@ -1,15 +1,24 @@
+import React from 'react'
 import { Animator, AnimatorGeneralProvider } from '@arwes/animation'
 import { Figure, Text } from '@arwes/core'
+import styled from '@emotion/styled'
 
-import React from 'react'
+const StyledFigure = styled(Figure)`
+  .arwes-figure__asset {
+    background-color: #05c6c1;
+  }
+  .arwes-figure__image {
+    opacity: 0.8;
+  }
+`
 
 const Hero: React.FC = () => {
   return (
     <AnimatorGeneralProvider animator={{ duration: { enter: 600, exit: 200 } }}>
       <Animator animator={{ activate: true, manager: 'stagger' }}>
-        <Figure src={'/assets/img/cover.jpg'}>
+        <StyledFigure src={'/assets/img/cover.jpg'}>
           人間は完全に整っているものと、どこかが欠けているものが同時に視界に入ったとき、欠けているも方に注目してしまう。
-        </Figure>
+        </StyledFigure>
         <Text as="h3">Skills, Works and Philosophy</Text>
         <Text as="p">{/* TODO */}</Text>
         <Text as="p">{/* TODO */}</Text>
