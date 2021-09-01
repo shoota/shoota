e0d49a97700d22dea882a430c37d8a7d705b0481
Shoota Kumano <shoota.dev@gmail.com>
2021-09-01 22:44:28 +0900

    supress SSR useLayoutEffect warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,15 @@ import { AppProps } from 'next/app'
 
 import { ArwesThemeProvider, StylesBaseline } from '@arwes/core'
 
+// supress SSR warning
+/*
+  Warning:
+    useLayoutEffect does nothing on the server, because its effect cannot be encoded into the server renderer's output format.
+    This will lead to a mismatch between the initial, non-hydrated UI and the intended UI.
+    To avoid this, useLayoutEffect should only be used in components that render exclusively on the client. 
+ */
+React.useLayoutEffect = React.useEffect
+
 // import { theme } from '../theme'
 // import '../styles/index.css'
 
