6e75a573bfcb5b91d1eee6a0cc68dc6f1647224b
Shuta Kumano <shoota.dev@gmail.com>
2023-07-03 22:42:12 +0900

    fix: site name

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const HOME_OG_IMAGE_URL =
   'https://gist.github.com/shoota/9319742/raw/3d1a799e759fb548262cdf5484cc1a914e56e7bd/avt.jpg'
 
-export const SITE_NAME = 'shoota.work'
+export const SITE_NAME = 'shoota works'
