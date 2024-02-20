cab3416e176d4582262cf520caa51f7cbf94ede3
Shuta Kumano <shoota.dev@gmail.com>
2024-02-21 02:56:43 +0900

    feat: fix table on GFM

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,20 @@
-import remark from 'remark'
-import html from 'remark-html'
+import rehypeRaw from 'rehype-raw'
+import rehypeStringify from 'rehype-stringify'
+import { remark } from 'remark'
+import remarkBreaks from 'remark-breaks'
+import remarkGfm from 'remark-gfm'
+import remarkRehype from 'remark-rehype'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  // const result = await remark().use(remarkGfm).use(remarkHtml).process(markdown)
+  const result = await remark()
+    .use(remarkGfm)
+    .use(remarkBreaks)
+    .use(remarkRehype, {
+      allowDangerousHtml: true,
+    })
+    .use(rehypeRaw)
+    .use(rehypeStringify)
+    .process(markdown)
   return result.toString()
 }
