c17324df9de21ead0fefb22d30cb5476cfea20ab
Shoota Kumano <shoota.dev@gmail.com>
2024-02-21 23:34:49 +0900

    feat: change code tag style (#113)
    * feat: override code style
    
    * chore: change linter setting
    
    * feat: introduce rehypePrettyCode

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,3 +1,4 @@
+import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeRaw from 'rehype-raw'
 import rehypeStringify from 'rehype-stringify'
 import { remark } from 'remark'
@@ -5,8 +6,8 @@ import remarkBreaks from 'remark-breaks'
 import remarkGfm from 'remark-gfm'
 import remarkRehype from 'remark-rehype'
 
+// theme: https://github.com/shikijs/textmate-grammars-themes/tree/main/packages/tm-themes
 export default async function markdownToHtml(markdown: string) {
-  // const result = await remark().use(remarkGfm).use(remarkHtml).process(markdown)
   const result = await remark()
     .use(remarkGfm)
     .use(remarkBreaks)
@@ -14,6 +15,10 @@ export default async function markdownToHtml(markdown: string) {
       allowDangerousHtml: true,
     })
     .use(rehypeRaw)
+    .use(rehypePrettyCode, {
+      theme: 'poimandres',
+      keepBackground: true,
+    })
     .use(rehypeStringify)
     .process(markdown)
   return result.toString()
